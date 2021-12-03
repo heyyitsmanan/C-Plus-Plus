c754ad7a1addd82366e4ca6901669e792a34199b
heyyitsmanan <m.anan.ahuja2203@gmail.com>
2021-12-03 18:16:46 +0530

    Added primeOrNot Program

--- Programs/2.primeOrNot.cpp
+++ Programs/2.primeOrNot.cpp
@@ -11,10 +11,24 @@ int main()
     cin >> n;
     cout << endl;
 
+    bool isPrime = true;
+
     for (int i = 2; i < n; i++)
     {
         if (n % i == 0)
         {
+            isPrime = false;
+            break;
         }
+    }
+    if (isPrime)
+    {
+        cout << n << " is a prime number." << endl;
+        cout << endl;
+    }
+    else
+    {
+        cout << n << " is not a prime number." << endl;
+        cout << endl;
     }
 }
