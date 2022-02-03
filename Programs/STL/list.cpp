3601e7e824c304ee4e52f98df45edbdd862aded5
heyyitsmanan <m.anan.ahuja2203@gmail.com>
2022-02-03 09:14:52 +0530

    Adding next STL DS i.e. List

--- Programs/STL/list.cpp
+++ Programs/STL/list.cpp
@@ -5,5 +5,33 @@ using namespace std;
 
 int main()
 {
-    return 0;
+    list<int> l;
+
+    for (int i : l)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    l.push_back(4);
+    l.push_front(3);
+    l.push_front(2);
+    l.push_back(5);
+    l.push_front(1);
+
+    l.pop_front();
+    l.pop_back();
+
+    for (int i : l)
+    {
+        cout << i << " " << endl;
+    }
+
+    cout << endl;
+
+    if (l.empty())
+        cout << "Empty" << endl;
+    else
+        cout << "Not Empty" << endl;
 }
